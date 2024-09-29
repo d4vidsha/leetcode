bceb67811485ce8324e3da630b28e9626456af16
David Sha <david.sha2002@gmail.com>
2024-09-29 19:41:02 +1000

    feat: solved in a iterative approach
    A simpler approach is to use DFS.

---
+++ solutions/39-combination-sum.py
@@ -0,0 +1,35 @@
+
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = []
+        visited = set()
+        queue = collections.deque([])
+        
+        for i in range(len(nums)):
+            count = [0] * len(nums)
+            count[i] += 1
+            queue.append(count)
+        
+
+        while queue:
+            count = queue.popleft()
+            # calculate total
+            total = 0
+            for i in range(len(count)):
+                total += nums[i] * count[i]
+            # a result
+            if total == target and tuple(count) not in visited:
+                result = []
+                for i in range(len(count)):
+                    for _ in range(count[i]):
+                        result.append(nums[i])
+                res.append(result)
+                visited.add(tuple(count))
+            # not yet reached result
+            elif total <= target:
+                for i in range(len(nums)):
+                    new_count = count.copy()
+                    new_count[i] += 1
+                    queue.append(new_count)
+
+        return res
