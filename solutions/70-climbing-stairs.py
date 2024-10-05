f5ee1d85986c42a339a93a1a9e84a5f4334d1478
David Sha <david.sha2002@gmail.com>
2024-10-06 10:13:02 +1100

    feat: use leetcode solution

---
+++ solutions/70-climbing-stairs.py
@@ -0,0 +1,10 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [0] * (n + 1)
+        def subproblem(i):
+            nonlocal dp
+            if i == 0 or i == 1:
+                return 1 
+            dp[i] = subproblem(i - 1) + subproblem(i - 2)
+            return dp[i]
+        return subproblem(n)
