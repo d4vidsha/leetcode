f65507b64b18c0a9a8a4778680becd32c541d63f
David Sha <david.sha2002@gmail.com>
2024-11-18 11:31:12 +1100

    refactor: make prettier

--- solutions/323-number-of-connected-components-in-an-undirected-graph.py
+++ solutions/323-number-of-connected-components-in-an-undirected-graph.py
@@ -3,24 +3,26 @@ class Solution:
         par = [i for i in range(n)]
         rank = [1] * n
         numUnions = 0
+        
         def union(n1, n2):
             nonlocal numUnions
             r1 = find(n1)
             r2 = find(n2)
-            if r1 != r2:
-                if rank[r1] < rank[r2]:
-                    r1, r2 = r2, r1
-                numUnions += 1
-                par[r2] = r1
-                rank[r1] += 1
+            if r1 == r2:
+                return
+            if rank[r1] < rank[r2]:
+                r1, r2 = r2, r1
+            numUnions += 1
+            par[r2] = r1
+            rank[r1] += 1
             return
 
         def find(node):
             while node != par[node]:
+                par[node] = par[par[node]]
                 node = par[node]
             return node
 
         for n1, n2 in edges:
             union(n1, n2)
         return n - numUnions
-
