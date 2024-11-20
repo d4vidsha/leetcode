6259bf9407ccebe34a538fbe604cd46df3357226
David Sha <david.sha2002@gmail.com>
2024-11-20 21:53:44 +1100

    refactor: learnt about not using range

--- solutions/212-word-search-ii.py
+++ solutions/212-word-search-ii.py
@@ -3,6 +3,16 @@ class TrieNode:
         self.child = [None] * 26
         self.val = val
         self.end = False
+
+    def addWord(self, word):
+        curr = self
+        for c in word:
+            index = ord(c) - ord("a")
+            if curr.child[index] == None:
+                curr.child[index] = TrieNode(c)
+            curr = curr.child[index]
+        curr.end = True
+
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
         res = set()
@@ -12,40 +22,30 @@ class Solution:
         # build the Trie
         root = TrieNode()
         for word in words:
-            curr = root
-            for c in word:
-                index = ord(c) - ord("a")
-                if curr.child[index] == None:
-                    curr.child[index] = TrieNode(c)
-                curr = curr.child[index]
-            curr.end = True
+            root.addWord(word)
 
         # DFS
         visited = set()
-        construct = ""
-        def dfs(node, r, c):
-            nonlocal construct
-            print(construct)
+        def dfs(node, r, c, word):
             if (r, c) in visited:
+                return
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS:
                 return
             if (nextNode := node.child[ord(board[r][c]) - ord("a")]) == None:
                 return
             visited.add((r, c))
-            construct = construct + board[r][c]
             if nextNode.end == True:
-                res.add(construct)
+                res.add(word + board[r][c])
+            
             directions = ((0, 1), (0, -1), (1, 0), (-1, 0))
             for dr, dc in directions:
-                if r+dr not in range(ROWS) or c+dc not in range(COLS):
-                    continue
-                dfs(nextNode, r+dr, c+dc)
+                dfs(nextNode, r+dr, c+dc, word + board[r][c])
+        
             visited.remove((r, c))
-            construct = construct[:-1]
-
         # search through the graph
         for r in range(ROWS):
             for c in range(COLS):
-                dfs(root, r, c)
+                dfs(root, r, c, "")
 
         return list(res)
         
