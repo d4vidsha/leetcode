e08fcf26de7273714ef9c4b61c7884a3dae44a5e
David Sha <david.sha2002@gmail.com>
2025-01-05 13:45:21 +1100

    feat: solved the problem in a O(1) space
    This is still O(m*n) time complexity, but space complexity is now O(1)
    instead of O(m*n). This is after looking at the Neetcode solution for
    this problem.

--- solutions/73-set-matrix-zeroes.py
+++ solutions/73-set-matrix-zeroes.py
@@ -1,18 +1,35 @@
-
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
+        topRow = None
         rows = len(matrix)
         cols = len(matrix[0])
-        visited = set()
-        for x in range(cols):
-            for y in range(rows):
-                if matrix[y][x] != 0 or (x, y) in visited:
-                    continue
-                for i in range(rows):
-                    if matrix[i][x] != 0:
-                        matrix[i][x] = 0
-                        visited.add((x, i))
-                for i in range(cols):
-                    if matrix[y][i] != 0:
-                        matrix[y][i] = 0
-                        visited.add((i, y))
+
+        for c in range(cols):
+            if matrix[0][c] == 0:
+                topRow = 0
+                break
+        for c in range(cols):
+            for r in range(rows):
+                if matrix[r][c] == 0:
+                    matrix[0][c] = 0
+                    break
+        for r in range(1, rows):
+            for c in range(cols):
+                if matrix[r][c] == 0:
+                    matrix[r][0] = 0
+                    break
+
+        # add the zeroes
+        for r in range(1, rows) :
+            if matrix[r][0] == 0:
+                for c in range(1, cols):
+                    matrix[r][c] = 0
+        for c in range(cols):
+            if matrix[0][c] == 0:
+                for r in range(1, rows):
+                    matrix[r][c] = 0
+        if topRow == 0:
+            for c in range(cols):
+                matrix[0][c] = 0
+
+
