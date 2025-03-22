caa77a48ba5fc85af6d28298e76042c28b16f528
David Sha <david.sha2002@gmail.com>
2025-03-22 17:15:56 +1100

    feat: solved using topological sort

--- solutions/329-longest-increasing-path-in-a-matrix.py
+++ solutions/329-longest-increasing-path-in-a-matrix.py
@@ -1,24 +1,47 @@
+from collections import defaultdict, deque
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        # in adjacency list
+        adjList = defaultdict(list)
+
         rows = len(matrix)
         cols = len(matrix[0])
-        dp = [[0] * cols for _ in range(rows)]
-        directions = ((1, 0), (0, 1), (-1, 0), (0, -1))
-        def solve(r, c, prevVal):
-            if r < 0 or r >= rows or c < 0 or c >= cols or matrix[r][c] <= prevVal:
-                return 0
-            if dp[r][c] != 0:
-                return dp[r][c]
-
-            res = 0
-            for dr, dc in directions:
-                res = max(res, 1+solve(r + dr, c + dc, matrix[r][c]))
-            dp[r][c] = res
-            return res
-        
+        directions = ((1, 0), (-1, 0), (0, 1), (0, -1))
         for r in range(rows):
             for c in range(cols):
-                solve(r, c, -1)
+                for dr, dc in directions:
+                    if r+dr < 0 or r+dr >= rows or c+dc < 0 or c+dc >= cols:
+                        continue
+                    if matrix[r][c] < matrix[r+dr][c+dc]:
+                        adjList[(r+dr, c+dc)].append((r, c))
+        
+        # topological sort via kahn's algorithm
+        top_sort = []
+        indegree = defaultdict(int)
+        for node in adjList:
+            indegree[node] = len(adjList[node])
 
-        return max([max(i) for i in dp])
+        queue = deque([])
+        for r in range(rows):
+            for c in range(cols):
+                if indegree[(r, c)] == 0:
+                    queue.append((r, c))
+
+        while queue:
+            r, c = queue.pop()
+            top_sort.append((r, c))
+            for dr, dc in directions:
+                if r+dr < 0 or r+dr >= rows or c+dc < 0 or c+dc >= cols:
+                    continue
+                if matrix[r][c] < matrix[r+dr][c+dc]:
+                    indegree[(r+dr, c+dc)] -= 1
+                    if indegree[(r+dr, c+dc)] == 0:
+                        queue.append((r+dr, c+dc))
+        
+        # bottom up dynamic programming
+        dp = defaultdict(lambda: 1)
+        for node in top_sort:
+            for prev in adjList[node]:
+                dp[node] = max(dp[node], 1 + dp[prev])
+        return max(dp.values()) if dp else 1
 
