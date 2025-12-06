b8c8c15c900e31b2dade881c074fa037ee0a42a2
David Sha <davvdsha@amazon.com>
2025-12-06 18:33:51 +1100

    solve combination sum again

--- solutions/39-combination-sum.py
+++ solutions/39-combination-sum.py
@@ -1,15 +1,23 @@
 class Solution:
-    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        
         res = []
 
-        def dfs(i, comb, total):
-            if total == target:
-                res.append(comb)
-                return
-            if i == len(nums) or total > target:
-                return
-            dfs(i, comb + [nums[i]], total + nums[i])
-            dfs(i + 1, comb, total)
-        
-        dfs(0, [], 0)
+        candidates.sort()
+
+        def recursion(remaining, curr):
+            nonlocal res
+            if remaining > 0:
+                for c in candidates:
+                    if curr and curr[-1] <= c:
+                        recursion(remaining - c, curr + [c])
+                        return
+                    recursion(remaining - c, curr + [c])
+
+
+            elif remaining == 0:
+                res.append(curr)
+
+        recursion(target, [])
+
         return res
